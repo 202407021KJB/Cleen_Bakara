de3c8be6c9d2eda75d7a3fb1b6c2a411b2fe2915
202407021KJB <jongbum1111@yuhan.ac.kr>
2025-11-13 09:30:35 +0900

    기존 게임 로직은 둔 채 변수 부분 따로 분리

---
+++ src/main/java/model/LadderData.java
@@ -0,0 +1,36 @@
+package model;
+
+import java.util.List;
+
+public class LadderData {
+    private List<Integer> xPositions;
+    private List<Rung> rungs;
+
+    public static class Rung 
+    {
+        public int y;
+        public int col;
+
+        public Rung(int y, int col) 
+        {
+            this.y = y;
+            this.col = col;
+        }
+    }
+
+    public LadderData(List<Integer> xPositions, List<Rung> rungs)
+    {
+        this.xPositions = xPositions;
+        this.rungs = rungs;
+    }
+
+    public List<Integer> getXPositions()
+    {
+        return xPositions;
+    }
+
+    public List<Rung> getRungs() 
+    {
+        return rungs;
+    }
+}
